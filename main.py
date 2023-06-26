1d09a24dc16b8c628d8ce983ea2ad959e452d1cb
Rehan Akhtar <124514979+ryhan5@users.noreply.github.com>
2023-06-26 16:57:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+def moveZeroes(nums):
+    left = 0
+    right = 0
+
+    while right < len(nums):
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+        right += 1
+
+    nums[left:] = [0] * (len(nums) - left)
+
+# Example usage:
+nums = [0, 1, 0, 3, 12]
+moveZeroes(nums)
+print(nums)  # Output: [1, 3, 12, 0, 0]
